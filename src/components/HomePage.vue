55923764034a75e01e66e7252781953b703a4b7c
belaid alahian <alahian.belaid@gmail.com>
2023-06-22 23:23:40 +0100

    adding hero svg

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,12 +2,13 @@
 import SideBar from './SideBar.vue'
 import GithubIcon from '@/components/icons/GithubIcon.vue'
 import InIcon from '@/components/icons/InIcon.vue'
+import HeroSvg from './HeroSvg.vue'
 </script>
 <template>
   <div>
     <SideBar />
     <section class="h-screen bg-neutral-800 ml-16">
-      <div class="container flex items-center relative h-full">
+      <div class="container flex items-center gap-28 relative h-full">
         <div class="my-auto p-2">
           <h1 class="font-Audiowide font-semibold text-5xl text-white uppercase">
             I'm <span class="text-amber-500">belaid</span> Alahian
@@ -17,17 +18,19 @@ import InIcon from '@/components/icons/InIcon.vue'
             with a focus on user-friendly interfaces and clean code.
           </p>
         </div>
-        <div class="flex-cols gap-4 absolute bottom-0 p-1">
+        <div class="h-3/4">
+          <HeroSvg />
+        </div>
+        <div class="flex-cols gap-4 absolute bottom-0 px-1">
           <GithubIcon />
           <div class="flex justify-center p-1">
             <div class="h-3 w-px bg-amber-500"></div>
           </div>
           <InIcon />
-          <div class="flex justify-center p-1">
+          <div class="flex justify-center">
             <div class="h-20 w-px bg-amber-500"></div>
           </div>
         </div>
-        <div class=""></div>
       </div>
     </section>
   </div>
