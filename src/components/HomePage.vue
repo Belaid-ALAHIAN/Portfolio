ef808470f17242c8ce34798375c30ff4c24419da
belaid alahian <alahian.belaid@gmail.com>
2023-06-22 17:42:00 +0100

    adding icons + enhance side bar

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,30 +1,34 @@
 <script setup>
+import SideBar from './SideBar.vue'
 import GithubIcon from '@/components/icons/GithubIcon.vue'
 import InIcon from '@/components/icons/InIcon.vue'
 </script>
 <template>
-  <section class="h-screen bg-neutral-900">
-    <div class="container flex items-center relative h-full">
-      <div class="my-auto p-2">
-        <h1 class="font-Audiowide font-semibold text-5xl text-white uppercase">
-          I'm <span class="text-amber-500">belaid</span> Alahian
-        </h1>
-        <p class="font-Montserrat font-extralight text-white p-1">
-          Frontend developer creating engaging web experiences,<br />
-          with a focus on user-friendly interfaces and clean code.
-        </p>
-      </div>
-      <div class="flex-cols gap-4 absolute bottom-0 p-1">
-        <GithubIcon />
-        <div class="flex justify-center p-1">
-          <div class="h-3 w-px bg-amber-500"></div>
+  <div>
+    <SideBar />
+    <section class="h-screen bg-neutral-800 ml-16">
+      <div class="container flex items-center relative h-full">
+        <div class="my-auto p-2">
+          <h1 class="font-Audiowide font-semibold text-5xl text-white uppercase">
+            I'm <span class="text-amber-500">belaid</span> Alahian
+          </h1>
+          <p class="font-Montserrat font-extralight text-white p-1">
+            Frontend developer creating engaging web experiences,<br />
+            with a focus on user-friendly interfaces and clean code.
+          </p>
         </div>
-        <InIcon />
-        <div class="flex justify-center p-1">
-          <div class="h-20 w-px bg-amber-500"></div>
+        <div class="flex-cols gap-4 absolute bottom-0 p-1">
+          <GithubIcon />
+          <div class="flex justify-center p-1">
+            <div class="h-3 w-px bg-amber-500"></div>
+          </div>
+          <InIcon />
+          <div class="flex justify-center p-1">
+            <div class="h-20 w-px bg-amber-500"></div>
+          </div>
         </div>
+        <div class=""></div>
       </div>
-      <div class=""></div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HomeIcon from './icons/HomeIcon.vue'
+import AboutIcon from './icons/AboutIcon.vue'
+import CodeIcon from './icons/CodeIcon.vue'
+</script>
+<template>
+  <div class="w-16 h-screen fixed bg-neutral-900">
+    <ul class="h-full px-4 w-full flex flex-col justify-center gap-2">
+      <li>
+        <HomeIcon class="stroke-white cursor-pointer hover:stroke-amber-500" />
+      </li>
+      <li><AboutIcon class="stroke-white cursor-pointer hover:stroke-amber-500" /></li>
+      <li><CodeIcon class="stroke-white cursor-pointer hover:stroke-amber-500" /></li>
+    </ul>
+  </div>
+</template>
